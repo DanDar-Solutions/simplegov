1b12df541700dabe5d19295b6887e19916e17082
Test User <user@example.com>
2025-11-08 10:44:53 +0800

    after changes

--- src/components/web/widgets/NewsWidget.tsx
+++ src/components/web/widgets/NewsWidget.tsx
@@ -2,11 +2,20 @@
 
 import NewsFeed from "@/components/web/legits/NewFeeds";
 import type { ModernNewsItem } from "@/static/news/modern";
+import { MicrophoneIcon } from '@heroicons/react/24/solid';
 
 interface HomeWidgetProps {
   news: ModernNewsItem[];
 }
 
 export default function HomeWidget({ news }: HomeWidgetProps) {
-  return <NewsFeed news={news} />;
+  return (
+    <>
+      {/* add some big microphone logo */}
+      <div className="flex flex-col items-center gap-8 mt-10">
+        <MicrophoneIcon className="w-16 h-16 text-red-600 border rounded-full p-2 " />
+        <NewsFeed news={news} />
+      </div>
+    </>
+);
 }
